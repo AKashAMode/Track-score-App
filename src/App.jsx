8721ef336ad5c9d377fa91c8bde89a0f9960cbe5
AKashAMode <akashamode80@gmail.com>
2025-02-18 15:18:44 +0530

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import ScoreCard from "./components/ScoreCard"
+import toast, { Toaster } from 'react-hot-toast';
 
 
 const App = () => {
 
   const [scoreTeamA, setScoreTeamA] = useState(0);
   const [scoreTeamB, setScoreTeamB] = useState(0);
-
+  const [winner, setWinner] = useState(null);
+  const MAX_SCORE = 10;
 
   useEffect(() => {
     
-    if(scoreTeamA < 10 && scoreTeamB < 10){
+    if(scoreTeamA < MAX_SCORE && scoreTeamB < MAX_SCORE){
       return
     }
 
-    if(scoreTeamA == 10){
-      alert("Team A is Win !!!!")
+    if(scoreTeamA == MAX_SCORE){
+      setWinner("Team A");
+      toast.success("Team A is Win !!!!")
     }
     
-    if(scoreTeamB == 10){
-      alert("Team B is Win !!!!")
+    if(scoreTeamB == MAX_SCORE){
+      setWinner("Team B");
+      toast.success("Team B is Win !!!!")
     }
 
 
@@ -27,33 +31,38 @@ const App = () => {
 
 
   return (
-    <div>
-      <div>
+    <div className='flex justify-center align-center my-10'>
+      <div style={{backgroundColor:"#007a66"}} className='p-4 rounded w-[95%]'>
       <h1 className='text-center text-4xl md:text-6xl  font-bold py-1 md:py-4'>Score Board</h1>
         <div className='flex justify-evenly flex-wrap m-14'>
          <ScoreCard score={scoreTeamA} teamName="Team A" 
-         decreaseScore={()=> {setScoreTeamA(scoreTeamA - 1)}} 
-         increaseScore={()=> {setScoreTeamA(scoreTeamA + 1)}}/>
+         decreaseScore={()=> {setScoreTeamA(scoreTeamA >= 1 ? scoreTeamA - 1 : 0)}} 
+         increaseScore={()=> {setScoreTeamA(scoreTeamA + 1)}}
+         winner={winner}
+         />
 
          <ScoreCard score={scoreTeamB} teamName="Team B" 
-           decreaseScore={()=> {setScoreTeamB(scoreTeamB - 1)}}
+           decreaseScore={()=> {setScoreTeamB(scoreTeamB >= 1 ? scoreTeamB - 1 : 0)}}
            increaseScore={()=> {setScoreTeamB(scoreTeamB + 1)}}
+           winner={winner}
          />
         
 
         </div>
+        {winner ? (<p className='text-center pb-12 text-2xl text-white'><span className='font-bold underline decoration-wavy pr-2'>{winner}</span> is Winner 🏆🏆 Click on Reset Button to Start Again</p>):null}
         <div>
           <button className='bg-teal-600 px-8 rounded mb-8 mx-auto block cursor-pointer text-white py-2 mx-auto text-2xl'
           onClick={()=> {
             setScoreTeamA(0)
             setScoreTeamB(0);
+            setWinner(null);
           }}
           >Reset Score</button>
         </div>
       </div>
-
+       <Toaster/>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -5,18 +5,21 @@ import {CircleMinus as IconCircleMinus, CirclePlus as IconCirclePlus } from "luc
 
 
 
-const ScoreCard = ({score, increaseScore, decreaseScore, teamName}) => {
+const ScoreCard = ({score, increaseScore, decreaseScore, teamName, winner}) => {
+  const isWinner = winner == teamName;
   return (
     <div className='border-none rounded-2xl p-4 w-[500px] h-[250px] bg-teal-800 m-2'>
-           <h2 className='text-center text-4xl font-bold text-white'>{teamName}</h2>
+           <h2 className='text-center text-4xl font-bold text-white'>{teamName}{isWinner ? "🏆":""}</h2>
            <h3 className='text-center text-6xl py-2 md:text-8xl font-bold text-white'>{score}</h3>
-           <div className='flex justify-between px-8'>
+
+           {winner ? null:
+           (<div className='flex justify-between px-8'>
             <IconCircleMinus color='#fff' className='cursor-pointer' size={65}
              onClick={decreaseScore}
             />
             <IconCirclePlus color='#fff' className='cursor-pointer' size={65}
             onClick={increaseScore}/>
-           </div>
+           </div>)}
           </div>
   )
 }
